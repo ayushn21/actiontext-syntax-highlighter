9ecb3398341c1a6a40bf604fdddb766f87ed6bf4
Ayush Newatia <ayush@hey.com>
2021-05-31 20:54:14 +0100

    Fix engine references

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "actiontext/syntax/highlighter"
+require "action_text_syntax_highlighter"
 
 module Dummy
   class Application < Rails::Application
