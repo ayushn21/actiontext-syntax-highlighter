adf96ba807bd0f6eb4adeab8cfabc05f3e90df56
Ayush Newatia <ayush@hey.com>
2021-05-31 23:16:15 +0100

    Remove default test file

--- test/action_text_syntax_highlighter/highlighter_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Actiontext::Syntax::HighlighterTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Actiontext::Syntax::Highlighter::VERSION
-  end
-end
